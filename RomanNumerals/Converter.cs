2fffeb4c4f1f9d083547df4ef6551ca54fdda3fe
miana <miana@claycorp.com>
2019-09-05 17:45:01 -0500

    incomplete

---
+++ RomanNumerals/Converter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RomanNumerals
+{
+    public class Converter
+    {
+        public string Convert(int number)
+        {
+            if(number == 2)
+            {
+                return "II";
+            }
+            else
+            {
+                return "I";
+            }
+
+        }
+
+    }
+}
---
+++ RomanNumeralsTest/RomanNumeralsTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using RomanNumerals;
+using System;
+
+namespace RomanNumeralsTest
+{
+    public class RomanNumeralsTest
+    {
+        [Test]
+        public void createConverter()
+        {
+            Converter Converter = new Converter();
+        }
+
+
+        [Test]
+        public void convert_1_Roman_Numeral()
+        {
+            Converter Converter = new Converter();
+
+            Converter.Convert(1);
+
+            Assert.AreEqual("I", Converter.Convert(1));
+        }
+
+        [Test]
+        public void convert_2_Roman_Numeral()
+        {
+            Converter Converter = new Converter();
+
+            Converter.Convert(2);
+
+            Assert.AreEqual("II", Converter.Convert(2));
+        }
+
+    }
+}
