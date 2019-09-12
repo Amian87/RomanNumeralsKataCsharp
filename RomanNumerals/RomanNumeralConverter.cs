8ca0e154326e49f9cef266d86f9986a9c6ff7e1d
Abdulla Mian <miana@claycorp.com>
2019-09-12 11:21:42 -0500

    incomplete

---
+++ RomanNumerals/RomanNumeralConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace RomanNumerals
+{
+    public class RomanNumeralConverter
+    {
+        public string ConvertToRoman(int number)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < number; i++)
+            {
+                sb.Append("I");
+            }
+            
+            return sb.ToString(); 
+              
+        }
+    }
+}
---
+++ RomanNumeralsTest/RomanNumeralsTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using RomanNumerals;
+using System;
+
+namespace RomanNumeralsTest
+{
+    public class RomanNumeralsTests
+    {
+        RomanNumeralConverter romanNumeralConverter = new RomanNumeralConverter();
+
+        [SetUp]
+        public void createRomanNumeralConverter()
+        {
+            romanNumeralConverter = new RomanNumeralConverter();
+        }
+
+        [Test]
+        public void convert_1_2_3_ToRomanNumeral()
+        {
+            Assert.AreEqual("I", romanNumeralConverter.ConvertToRoman(1));
+            Assert.AreEqual("II", romanNumeralConverter.ConvertToRoman(2));
+            Assert.AreEqual("III", romanNumeralConverter.ConvertToRoman(3));
+            Assert.AreEqual("V", romanNumeralConverter.ConvertToRoman(4));
+
+        }
+
+    }
+}
