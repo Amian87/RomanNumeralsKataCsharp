e2a4736037f8f6d8ca49eab16f7a9453a86ec5f2
Abdulla Mian <miana@claycorp.com>
2019-09-13 16:12:44 -0500

    refactored test

---
+++ RomanNumeralTests/RomanNumeralsTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using RomanNumeralsKata;
+using System;
+
+namespace RomanNumeralTests
+{
+    public class RomanNumeralsTest
+    {
+        RomanNumerals rn = new RomanNumerals();
+
+        [SetUp]
+        public void SetUpRomanNumerals()
+        {
+            rn = new RomanNumerals();
+        }
+
+        [TestCase("I",1)]
+        [TestCase("II", 2)]
+        [TestCase("III", 3)]
+        [TestCase("IV", 4)]
+        [TestCase("V", 5)]
+        [TestCase("VI", 6)]
+        [TestCase("VII", 7)]
+        [TestCase("VIII", 8)]
+        [TestCase("IX", 9)]
+        public void ConvertToRomanNumeral(string roman, int arabic)
+        {
+            Assert.AreEqual(roman, rn.ConvertToRoman(arabic));
+        }
+
+
+
+    }
+}
---
+++ RomanNumeralsKata/RomanNumerals.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace RomanNumeralsKata
+{
+    public class RomanNumerals
+    {
+        public string ConvertToRoman(int arabic)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int remaining = arabic;
+
+            if(remaining == 9)
+            {
+                sb.Append("IX");
+                remaining -= 9;
+            }
+            if(remaining >= 5)
+            {
+                sb.Append("V");
+                remaining -= 5;
+            }
+            if(remaining == 4)
+            {
+                sb.Append("IV");
+                remaining -= 4;
+            }
+
+            for(int i = 0; i < remaining; i++)
+            {
+                sb.Append("I");
+            }
+
+            return sb.ToString();
+            
+        }
+        
+    }
+}
