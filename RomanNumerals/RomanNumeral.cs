d4c1037cb1de7de8a3c65d80e156ff57696f88f7
Abdulla Mian <miana@claycorp.com>
2019-09-13 20:15:52 -0500

    complete

---
+++ RomanNumerals/RomanNumeral.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RomanNumerals
+{
+    public class RomanNumeral
+    {
+        static Dictionary<int, string> arabicToRoman = new Dictionary<int, string>
+        {
+            { 1000, "M" },
+            { 900, "CM" },
+            { 500, "D" },
+            { 400, "CD" },
+            { 100, "C" },
+            { 90, "XC" },
+            { 50, "L" },
+            { 40, "XL" },
+            { 10, "X" },
+            { 9, "IX" },
+            { 6, "VI" },
+            { 5, "V" },
+            { 4, "IV" },
+            { 1, "I" }
+
+        };
+
+
+
+        public string ConvertToRoman(int arabic)
+        {
+
+
+            string roman = string.Empty;
+
+            foreach(int value in arabicToRoman.Keys)
+            {
+                while(arabic >= value)
+                {
+                    roman += arabicToRoman[value];
+                    arabic -= value;
+                }
+            }
+
+            return roman;
+
+        }
+
+    }
+}
---
+++ RomanNumeralsTest/RomanNumeralsTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using RomanNumerals;
+using System;
+
+namespace RomanNumeralsTest
+{
+    public class RomanNumeralsTest
+    {
+        RomanNumeral rn = new RomanNumeral();
+
+        [SetUp]
+        public void SetupRomanNumerals()
+        {
+            rn = new RomanNumeral();
+        }
+
+        [TestCase("I", 1)]
+        [TestCase("II", 2)]
+        [TestCase("III", 3)]
+        [TestCase("IV", 4)]
+        [TestCase("V", 5)]
+        [TestCase("VI", 6)]
+        [TestCase("IX", 9)]
+        public void ConvertOneTwoThreeToRomanNumeral(string numeral, int arabic)
+        {
+            Assert.AreEqual(numeral, rn.ConvertToRoman(arabic));
+        }
+    }
+}
