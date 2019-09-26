0b30e47b0dc5c22044be0e7d795ce05aa1ce916f
Abdulla Mian <miana@claycorp.com>
2019-09-26 12:11:58 -0500

    incomplete

---
+++ RomanNumerals/RomanNumeral.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace RomanNumerals
+{
+    public class RomanNumeral
+    {
+        public string ConvertToRoman(int arabic)
+        {
+            StringBuilder sb = new StringBuilder();
+            int remaining = arabic;
+         
+
+            if(remaining >= 5)
+            {
+                sb.Append("V");
+                remaining -= 5;
+            }
+           else if(remaining == 4)
+            {
+                sb.Append("IV");
+                remaining -= 4;
+            }
+            for(int i = 0; i < remaining; i++ )
+            {
+                sb.Append("I");
+            }
+
+            return sb.ToString();
+
+        }
+    }
+}
---
+++ RomanNumeralsTest/RomanNumeralsTest.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using RomanNumerals;
+using System;
+
+namespace RomanNumeralsTest
+{
+    public class RomanNumeralsTest
+    {
+        private RomanNumeral rn = new RomanNumeral();
+
+        [SetUp]
+        public void SetUpRomanNumerals()
+        {
+            rn = new RomanNumeral();
+        }
+
+        [Test]
+        public void ConvertOneToRomanNumeral()
+        {
+            Assert.AreEqual("I", rn.ConvertToRoman(1));
+            Assert.AreEqual("II", rn.ConvertToRoman(2));
+            Assert.AreEqual("III", rn.ConvertToRoman(3));
+        }
+
+        [Test]
+        public void ConvertFourToRomanNumeral()
+        {
+            Assert.AreEqual("IV", rn.ConvertToRoman(4));
+        }
+
+        [Test]
+        public void ConvertFiveToRomanNumeral()
+        {
+            Assert.AreEqual("V", rn.ConvertToRoman(5));
+        }
+
+        [Test]
+        public void ConvertSixSevenEightToRomanNumeral()
+        {
+            Assert.AreEqual("VI", rn.ConvertToRoman(6));
+            Assert.AreEqual("VII", rn.ConvertToRoman(7));
+            Assert.AreEqual("VIII", rn.ConvertToRoman(8));
+        }
+
+        [Test]
+        public void ConvertNineToRomanNumeral()
+        {
+            Assert.AreEqual("XI", rn.ConvertToRoman(9));
+        }
+
+    }
+}
